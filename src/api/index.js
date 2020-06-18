58ff57db85c835d70d39f6b261d8bff758d9de3a
mohamadbozorgi <mohamadbozorgi40@gmail.com>
2020-06-18 15:56:33 +0430

    working on fetch api

---
+++ src/api/index.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+import store from '../store'
+import {api_url} from '../constants'
+
+const fetch = new Promise((resolve, reject) => (url, method, data, hasFile) => {
+  let config = {
+    method: method || 'GET',
+    url: api_url + url,
+    headers: {
+      'Content-Type': hasFile ? 'multipart/form-data' : 'application/json',
+    }
+  }
+
+  const token = store.getState().user.token
+  if (token) {
+    config.headers.Authorization = 'Token ' + token
+  }
+
+  if (hasFile) {
+    const formData = new FormData()
+
+    for (key in data) {
+      
+    }
+  }
+
+  axios(config).then(res => {
+    resolve(res)
+  }).catch(error => {
+    reject(error)
+  })
+})
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const base_url = 'http://185.97.117.125/'
+export const api_url = base_url + 'api/v1/'
