8c07d03313507ec4840f894d7423710e718f89e2
mohamadbozorgi <mohamadbozorgi40@gmail.com>
2020-07-07 20:12:22 +0430

    use hooks in add new post screeen

--- src/screens/AddPostScreen.js
+++ src/screens/AddPostScreen.js
@@ -1,40 +1,23 @@
-import * as React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Button, Image, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import Constants from 'expo-constants';
 import * as Permissions from 'expo-permissions';
 
-export default class AddPostScreen extends React.Component {
-  state = {
-    image: null,
-  };
 
-  render() {
-    let { image } = this.state;
+const AddPostScreen = () => {
+  const [image, setImage] = useState(null)
 
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button title="Pick an image from camera roll" onPress={this._pickImage} />
-        <Button title="Take a Photo" onPress={this._takePhoto} />
-        {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
-      </View>
-    );
-  }
-
-  componentDidMount() {
-    this.getPermissionAsync();
-  }
-
-  getPermissionAsync = async () => {
+  const getPermissionAsync = async () => {
     if (Constants.platform.ios) {
       const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
       if (status !== 'granted') {
         alert('Sorry, we need camera roll permissions to make this work!');
       }
     }
-  };
+  }
 
-  _pickImage = async () => {
+  const pickImage = async () => {
     try {
       let result = await ImagePicker.launchImageLibraryAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.All,
@@ -52,7 +35,7 @@ export default class AddPostScreen extends React.Component {
     }
   };
 
-  _takePhoto = async () => {
+  const takePhoto = async () => {
     try {
       let result = await ImagePicker.launchCameraAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.All,
@@ -68,5 +51,19 @@ export default class AddPostScreen extends React.Component {
     } catch (E) {
       console.log(E);
     }
-  };
+  }
+
+  useEffect(()=> {
+    getPermissionAsync()
+  },[])
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      <Button title="Take a Photo" onPress={takePhoto} />
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+    </View>
+  )
 }
+
+export default AddPostScreen
