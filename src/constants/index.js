b1f663c10d0d5bd052b51190e4be436849d158fe
Vahid Al <thevahidal@gmail.com>
2020-07-15 18:35:51 +0430

    settings updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
 // export const base_url = 'http://localhost:8000/'
-export const base_url = 'http://185.97.117.125/'
+export const base_url = 'https://fellara.com/'
 export const api_url = base_url + 'api/v1/'
