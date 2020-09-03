abaf95a644bce30000831c552b247fd9183b406c
Vahid Al <thevahidal@gmail.com>
2020-09-04 03:48:55 +0430

    offline mode enabled

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 module.exports = async function(env, argv) {
     const config = await createExpoWebpackConfigAsync({
         ...env,
+        offline: true,
         babel: {
             dangerouslyAddModulePathsToTranspile: ['@ui-kitten/components']
         },
